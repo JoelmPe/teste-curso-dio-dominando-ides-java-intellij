25c5c49ce09c4503f7f5eb8f69a2066f7a39cb48
Joelma Pereira <joelma.sp@outlook.com.br>
2022-03-04 16:18:23 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,10 +8,47 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Uma mente milionária ", 388);
+
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
 
         System.out.println("Hello World!"  + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numpgns;
+
+    public Livro(String nome, Integer numpgns) {
+        this.nome = nome;
+        this.numpgns = numpgns;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpgns() {
+        return numpgns;
+    }
+
+    public void setNumpgns(Integer numpgns) {
+        this.numpgns = numpgns;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpgns=" + numpgns +
+                '}';
     }
 }
